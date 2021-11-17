b9b5969da1fa7b1c18e4b5d33f3be75e01a3eb83
Alex Kale <kalea@uw.edu>
2021-11-17 11:15:58 -0600

    fixed logistic regression model check

--- R/logistic.R
+++ R/logistic.R
@@ -1,4 +1,4 @@
-logistic_model_check <- function(mu_spec, sigma_spec = "~1", data) {
+logistic_model_check <- function(mu_spec, data) {
   library(tidyverse)
   library(gamlss)
   
@@ -21,56 +21,33 @@ logistic_model_check <- function(mu_spec, sigma_spec = "~1", data) {
     # replace log({{var}}) with log_{{var}} in mu_spec
     mu_spec <- str_replace_all(mu_spec, paste("log\\(", var_name, "\\)", sep = ""), paste("log_", var_name, sep = ""))
   }
-  log_trans_vars_sigma <- str_match_all(sigma_spec, "log\\(\\s*(.*?)\\s*\\)")[[1]][,2]
-  for (var_name in log_trans_vars_sigma) {
-    # compute log transform of variable and add to dataframe
-    var <- sym(var_name)
-    data <- data %>%
-      mutate(
-        "{{var}}" := if_else({{var}}==0.0,
-                             0.001, # avoid -inf errors by fudging the zeros a bit
-                             {{var}}
-        ),
-        "log_{{var}}" := log({{var}})
-      )
-    # replace log({{var}}) with log_{{var}} in sigma_spec
-    sigma_spec <- str_replace_all(sigma_spec, paste("log\\(", var_name, "\\)", sep = ""), paste("log_", var_name, sep = ""))
-  }
   
   # fit model
   mu_spec <- as.formula(mu_spec)
-  sigma_spec <- as.formula(sigma_spec)
-  model <- eval(bquote(gamlss(.(mu_spec), sigma.fo = .(sigma_spec), data = data, family = LO)))
+  model <- eval(bquote(gamlss(.(mu_spec), data = data, family = BI)))
   
   # get summary statistics describing model predictions
-  pred.mu <- predict(model, se.fit = TRUE)
-  pred.sigma <- predict(model, se.fit = TRUE, what = "sigma")
+  pred <- predict(model, se.fit = TRUE)
   output <- data %>%
     mutate(
-      logitmu.expectation = pred.mu$fit,                    # add fitted logmu and standard errors to dataframe
-      logitmu.se = pred.mu$se.fit,
-      df = df.residual(model),                            # get degrees of freedom
-      logsigma.expectation = pred.sigma$fit,              # add fitted logsigma and standard errors to dataframe 
-      logsigma.se = pred.sigma$se.fit
+      logitmu.expectation = pred$fit,                     # add fitted logmu and standard errors to dataframe
+      logitmu.se = pred$se.fit,
+      df = df.residual(model)                             # get degrees of freedom
     )
   
   # propagate uncertainty in fit to generate an ensemble of model predictions (mimic a posterior predictive distribution)
   output <- output %>%
     mutate(
       .draw = list(1:n_draws),                            # generate list of draw numbers
-      t1 = map(df, ~rt(n_draws, .)),                      # simulate draws from t distribution to transform into logit mu
-      t2 = map(df, ~rt(n_draws, .)),                      # simulate draws from t distribution to transform into log sigma
+      t = map(df, ~rt(n_draws, .))                        # simulate draws from t distribution to transform into logit mu
     ) %>%
-    unnest(cols = c(".draw", "t1", "t2")) %>%
+    unnest(cols = c(".draw", "t")) %>%
     mutate(
-      logitmu = t1 * logitmu.se + logitmu.expectation,    # scale and shift t to get a sampling distribution of logit mu
-      logsigma = t2 * logsigma.se + logsigma.expectation, # scale and shift t to get a sampling distribution of log sigma
-      sigma = exp(logsigma)                               # backtransform to sampling distribution of sigma
+      logitmu = t * logitmu.se + logitmu.expectation,     # scale and shift t to get a sampling distribution of logit mu
+      mu = plogis(logitmu)                                # backtransform to sampling distribution of mean probability
     ) %>%
     rowwise() %>%
     mutate(
-      logitp = rLO(1, logitmu, sigma),                    # sample a probability in logit units
-      p = plogis(logitp),                                 # backtransform to probability units
-      prediction = rbinom(1, 1, p)                        # compute predictive distribution of binary outcomes
+      prediction = rbinom(1, 1, mu)                       # compute predictive distribution of binary outcomes
     )
 }
