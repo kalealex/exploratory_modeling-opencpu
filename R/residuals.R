b494783965630a8f730e03e32f9d307bb02c37a3
Alex Kale <kalea@uw.edu>
2022-01-11 17:08:38 -0600

    added calc_residuals function

---
+++ R/residuals.R
@@ -0,0 +1,41 @@
+calc_residuals <- function(df, outcome_name) {
+
+    # read data from json
+    df <- fromJSON(df, simplifyVector = TRUE)
+
+    # outcome name as symbol
+    outcome_name <- sym(outcome_name)
+
+    # put data in wide format
+    df_wide <- df %>% pivot_wider(names_from = modelcheck_group, values_from = !!outcome_name)
+    
+    # get list of models whose predictions are included in this dataframe
+    model_names <- setdiff(names(df_wide),c(names(df), "data"))
+
+    # model and residual names as symbols
+    residual_names <- lapply(model_names, function (model) { sym(paste("res", model, sep = "| ")) })
+    model_names <- lapply(model_names, sym)
+
+    # iterate through models
+    for (i in 1:length(model_names)) {
+        # index for current model
+        residual_name <- residual_names[[i]]
+        model_name <- model_names[[i]]
+
+        # calculate residual
+        df_wide <- df_wide %>%
+            mutate(
+                !!residual_name := data - !!model_name
+            )
+    }
+
+    # put data back into long format for output
+    output <- df_wide %>%
+        pivot_longer(
+        cols = c("data", sapply(model_names, as.character), sapply(residual_names, as.character)),
+        names_to = "modelcheck_group",
+        values_to = as.character(outcome_name)
+    )
+
+    return(list(message = "success", data = toJSON(output)))
+}
