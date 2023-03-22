f833fa17abc83dfdf4eddbec9b7ed93379b320f0
Alex Kale <kalea@uchicago.edu>
2023-03-22 13:48:19 -0500

    removing stringr dependency

--- R/evm.R
+++ R/evm.R
@@ -70,7 +70,7 @@ add_model <- function(df, models, outcome_name, residuals = TRUE) {
                 )
             )
         # replace {{outcome_name}} with log_{{outcome_name}}
-        mu_spec <- str_replace_all(mu_spec, as.character(outcome_name), paste("log", outcome_name, sep = "_"))
+        mu_spec <- sub(as.character(outcome_name), paste("log", outcome_name, sep = "_"), mu_spec)
         
         # fit model
         mu_spec <- as.formula(mu_spec)
@@ -136,8 +136,8 @@ add_model <- function(df, models, outcome_name, residuals = TRUE) {
                                                   
                 )
             )
-        # replace {{outcome_name}} with log_{{outcome_name}}
-        mu_spec <- str_replace_all(mu_spec, as.character(outcome_name), paste("logit", outcome_name, sep = "_"))
+        # replace {{outcome_name}} with logit_{{outcome_name}}
+        mu_spec <- sub(as.character(outcome_name), paste("logit", outcome_name, sep = "_"), mu_spec)
         
         # fit model
         mu_spec <- as.formula(mu_spec)
